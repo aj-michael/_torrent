9580d02a6943fcff5c8603db46ad34f0e1a64143
Adam <adam@ajmichael.net>
2015-06-27 21:54:48 -0700

    Initial commit.

---
+++ src/main/java/net/ajmichael/_torrent/App.java
@@ -0,0 +1,13 @@
+package net.ajmichael._torrent;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
